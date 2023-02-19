50b1d74d9aafd6a5b220546888c78ae31243111f
Austin <75064473+amsierco@users.noreply.github.com>
2023-02-19 08:40:29 -0500

    Added submit button functionality and a logo for the splash text

--- javascript.js
+++ javascript.js
@@ -3,42 +3,49 @@ function checkPasswordMatch(){
     let password_1 = document.getElementById("password").value;
     let password_2 = document.getElementById("confirm-password").value;
     if(password_1 != password_2){
-        console.log("Passwords Do Not Match!");
         password_1 = null;
         password_2 = null;
         return false;
     }
     else
     {
-        console.log("Passwords Match!");
         return true;
     }
 }
 
-//Adds button functionality
+//Adds submit button functionality
 const submit_btn = document.querySelector(".btn")
 submit_btn.addEventListener("click", () => {
-    //checkPasswordMatch();
     validateForm();
 });
 
 //Validate Form
 function validateForm(){
     let form_array = document.querySelectorAll(".input input");
-    for(let i=0; i<form_array.length; i++){
-        console.log(form_array[i].value);
-        if(form_array[i].value == ""){
 
-            let node_id = document.getElementById("form_array[i].getAttribute('id')");
-            console.log(node_id);
-            node_id.style.cssText= 'background-color: green;';//'border-color: green;';
+    //Initially reset visual properties
+    for(let i=0; i<form_array.length; i++){
+        form_array[i].style.borderColor= "#000";
+    }
+
+    //Checks Validity
+    for(let i=0; i<form_array.length; i++){
+        //Empty field
+        if(form_array[i].value == ""){
+            form_array[i].style.borderColor= "red";
+        }
+        //Valid format
+        else if(form_array[i].checkValidity()){
+            form_array[i].style.borderColor= "green";
+        }
+        else{
+            form_array[i].style.borderColor= "red";
         }
     }
-}
 
-//Update UI
-function updateUI(){
-    if(!validateForm){
-
+    //Password match
+    if(!checkPasswordMatch()){
+        form_array[(form_array.length)-1].style.borderColor= "red";
+        form_array[(form_array.length)-2].style.borderColor= "red";
     }
 }
